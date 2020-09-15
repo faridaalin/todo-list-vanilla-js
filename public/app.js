e4e980376eb3bce988a8ae5b3bca9c65be45742d
Farida <farida.alin@yahoo.com>
2020-09-15 09:35:55 +0200

    refactured code

--- public/app.js
+++ public/app.js
@@ -1,87 +1,62 @@
-const dateContainer = document.querySelector(".date");
+const form = document.querySelector(".form");
 const input = document.querySelector(".todo-input");
 const button = document.querySelector(".todo-btn");
 const list = document.querySelector(".todo-list");
 
-let todoItems = [];
+const dateContainer = document.querySelector(".date");
 
-const getinputValue = () => {
-  const inputValue = input.value.trim();
-  if (inputValue === "") {
-    return;
-  } else {
-    todoItems.push({ item: inputValue, completed: false });
+let todos = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  addTodo(input.value);
+});
+
+const addTodo = (item) => {
+  if (item !== "") {
+    const todo = {
+      name: item,
+      completed: false,
+    };
+
+    todos.push(todo);
+    addToLocalStorage(todos);
+    input.value = "";
   }
-
-  input.value = "";
 };
 
-const addTodo = (todos) => {
-  if (todos.length > 0) {
-    list.innerHTML = "";
-    todos.map((item) => {
-      list.innerHTML += `<li class="todo-item ${
-        item.completed === true ? "finished" : ""
-      } flex items-center w-full pr-4">
+const renderTodos = (todos) => {
+  // clear items inside the <ul>
+  list.innerHTML = "";
+
+  // loop trough each todo
+  todos.forEach((item) => {
+    list.innerHTML += `<li class="todo-item ${
+      item.completed === true ? "finished" : null
+    } flex items-center w-full pr-4">
           <i class="completed ml-4 fa fa-check cursor-pointer" aria-hidden="true"></i>
-          <span class="ml-4">${item.item}</span>
+          <span class="ml-4">${item.name}</span>
           <i class="delete ml-auto cursor-pointer fa fa-trash" aria-hidden="true"></i>
       </li>`;
-    });
+  });
+};
+
+const addToLocalStorage = (todos) => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  renderTodos(todos);
+};
+
+const getFromLocalStorage = () => {
+  const localTodos = localStorage.getItem("todos");
+  if (localTodos) {
+    todos = JSON.parse(localTodos);
+    renderTodos(todos);
   }
-  markCompleteTodo();
-  deleteTodo();
-};
-const deleteTodo = () => {
-  const todosList = list.children;
-  const todosArray = [...todosList];
-  todosArray.forEach((item, index) => {
-    item.addEventListener("click", (event) => {
-      const { parentElement } = event.target;
-      if (event.target.className.includes("delete")) {
-        parentElement.remove();
-        todoItems.splice(index, 1);
-        saveToLocalStorage(todoItems);
-      } else {
-        return;
-      }
-    });
-  });
 };
 
-const markCompleteTodo = () => {
-  const todosList = list.children;
-  const todosArray = [...todosList];
-  todosArray.forEach((item, index) => {
-    item.addEventListener("click", (event) => {
-      const { parentElement } = event.target;
-      if (event.target.className.includes("completed")) {
-        todoItems[index].completed = !todoItems[index].completed;
-        parentElement.classList.toggle("finished");
-        saveToLocalStorage(todoItems);
-      } else {
-        return;
-      }
-    });
-  });
-};
-
-const displayTodo = () => {
-  // Get value from input field
-  getinputValue();
-
-  // add todo to the list
-  addTodo(todoItems);
-
-  // save to local storage on button click
-  saveToLocalStorage(todoItems);
-};
-
-button.addEventListener("click", displayTodo);
-input.addEventListener(
-  "keypress",
-  (e) => (e.key === "Enter" || e.keyCode === 13) && displayTodo()
-);
+getFromLocalStorage();
 
 // set date
 const addDate = () => {
@@ -111,43 +86,3 @@ const addDate = () => {
 };
 
 addDate();
-
-const getCompletedTodos = () => {
-  const completedTodos = todoItems.filter((item) => item.completed !== false);
-  addTodo(completedTodos);
-};
-const completedTasks = document.querySelector(".completed");
-completedTasks.addEventListener("click", getCompletedTodos);
-
-const getIncompleteTasks = () => {
-  const incomplTodos = todoItems.filter((item) => item.completed === false);
-  addTodo(incomplTodos);
-};
-const incompleteTasks = document.querySelector(".incompleted");
-incompleteTasks.addEventListener("click", getIncompleteTasks);
-
-const getAllTodos = () => {
-  addTodo(todoItems);
-};
-
-const all = document.querySelector(".all");
-all.addEventListener("click", getAllTodos);
-
-const saveToLocalStorage = (todos) => {
-  localStorage.setItem("todosList", JSON.stringify(todos));
-};
-
-const getFromLocalStorage = () => {
-  if (localStorage.getItem("todosList") === null) {
-    return;
-  } else {
-    todoItems = [...JSON.parse(localStorage.getItem("todosList"))];
-    addTodo(todoItems);
-  }
-};
-document.addEventListener("DOMContentLoaded", () => {
-  if (list.children) {
-    addTodo(todoItems);
-  }
-});
-getFromLocalStorage();
